10446d3182942e95f7340c9dbd30a23e4e794092
RubyTuesday77 <105758468+RubyTuesday77@users.noreply.github.com>
2022-09-23 18:06:49 -0400

    Adding 404 page route

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+// Get environment variables:
+require('dotenv').config()
+
+// Require needed node modules:
 const express = require('express')
+
+// Initialize app by calling function returned by the express module:
 const app = express()
 
+// Declare routes that can be accessed on app:
+// Create the home page route:
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(3000)
+
+// Create a 404 route:
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+
+// Call app.listen() to keep server open:
+app.listen(process.env.PORT)
