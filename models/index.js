718be60e434ad5ac60c875b5a752d8d971fa35b0
RubyTuesday77 <105758468+RubyTuesday77@users.noreply.github.com>
2022-10-07 15:00:41 -0400

    create and use seeder file

--- models/index.js
+++ models/index.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 
 // Connect to Mongoose:
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, 
+  useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
